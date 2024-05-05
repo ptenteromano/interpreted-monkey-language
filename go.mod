1d8b6187ec3ab6c49da800f1d1d9fc566434bc84
philip tenteromano <ptenteromano@justworks.com>
2024-05-05 10:10:31 -0400

    basic lexer test pass

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ptenteromano/monkey-interpreter
+
+go 1.22.1
---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, .
+	INT   = "INT"   // 1343456
+	BOOL  = "BOOL"  // true or false
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	BANG   = "!"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
