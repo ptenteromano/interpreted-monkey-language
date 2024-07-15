06e7ef51c9ac28239a3bf2b9f5844f86a876840f
philip tenteromano <ptenteromano@justworks.com>
2024-07-15 13:05:53 -0400

    use go project structure convention

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import "github.com/ptenteromano/monkey-interpreter/token"
+import "github.com/ptenteromano/monkey-interpreter/internal/token"
 
 type Lexer struct {
 	input        string
